133395a25ae80b51203de05d40b5fcfb77def9ce
Ryan Raeburn <ryanraeburn@gmail.com>
2015-02-22 19:35:04 -0800

    tests need fixed

--- test/villainsApi_test.js
+++ test/villainsApi_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-
 process.env.MONGO_URI = 'mongodb://localhost/villainsapp_test';
 require('../server.js');
+
 var chai = require('chai');
 var chaihttp = require('chai-http');
 var mongoose = require('mongoose');
@@ -10,10 +10,33 @@ var mongoose = require('mongoose');
 chai.use(chaihttp);
 
 var expect = chai.expect;
+var testToken;
 
 describe('villains api', function() {
+  
   after(function(done) {
     mongoose.connection.db.dropDatabase(function() {
+      done();
+    });
+  });
+
+  before(function(done) {
+    chai.request('localhost:3000/api/v1')
+      .post('/create_user')
+      .send({email: 'test@test', password: 'test'})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        done();
+      });
+  });
+
+  before(function(done) {
+    chai.request('localhost:3000/api/v1')
+    .post('/sign_in')
+    .send('-u test@test:test')
+    .end(function(err,res) {
+      expect(err).to.eql(null);
+      testToken = res.body.eat;
       done();
     });
   });
@@ -21,7 +44,7 @@ describe('villains api', function() {
   it('should respond to post', function(done) {
     chai.request('localhost:3000/api/v1')
     .post('/villains')
-    .send({realName: 'Guy McTest', superName: 'MadTester', publisher: 'testpeople'})
+    .send({ eat: testToken, realName: 'Guy McTest', superName: 'MadTester', publisher: 'testpeople'})
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.body).to.have.property('_id');
@@ -35,7 +58,7 @@ describe('villains api', function() {
   it('should have default realName', function(done) {
     chai.request('localhost:3000/api/v1')
     .post('/villains')
-    .send({superName: 'TestMan', publisher: 'Testers'})
+    .send({eat: testToken, superName: 'TestMan', publisher: 'Testers'})
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.body.realName).to.eql('Unknown');
@@ -48,7 +71,7 @@ describe('villains api', function() {
     beforeEach(function(done) {
       chai.request('localhost:3000/api/v1')
       .post('/villains')
-      .send({superName: 'Testzilla', publisher: 'TestGuy'})
+      .send({eat: testToken, superName: 'Testzilla', publisher: 'TestGuy'})
       .end(function(err, res) {
         id = res.body._id;
         done();
@@ -58,6 +81,7 @@ describe('villains api', function() {
     it('should have an index', function(done) {
       chai.request('localhost:3000/api/v1')
       .get('/villains')
+      .send(testToken)
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(Array.isArray(res.body)).to.eql(true);
@@ -69,7 +93,7 @@ describe('villains api', function() {
     it('should update a villain', function(done) {
       chai.request('localhost:3000/api/v1')
       .put('/villains/' + id)
-      .send({superName: 'Professor Test', publisher: 'Test Labs'})
+      .send({eat: testToken, superName: 'Professor Test', publisher: 'Test Labs'})
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.body.superName).to.eql('Professor Test');
@@ -81,6 +105,7 @@ describe('villains api', function() {
     it('should delete a villain', function(done) {
       chai.request('localhost:3000/api/v1')
       .delete('/villains/' + id)
+      .send(testToken)
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect((res.body)[0]._id).to.eql(id);
