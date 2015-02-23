16cf87a88e7e5db598302c4d9a266ca8c4163349
Ryan Raeburn <ryanraeburn@gmail.com>
2015-02-22 19:29:11 -0800

    added authentication

--- routes/villains_routes.js
+++ routes/villains_routes.js
@@ -1,12 +1,12 @@
 'use strict';
-
 var Villain = require('../models/SuperVillain');
+var eat_auth = require('../lib/eat_auth');
 var bodyparser = require('body-parser');
 
-module.exports = function(app) {
+module.exports = function(app, appSecret) {
   app.use(bodyparser.json());
 
-  app.get('/villains', function(req, res) {
+  app.get('/villains', eat_auth(appSecret), function(req, res) {
     Villain.find({}, function(err, data) {
       if(err) return res.status(500).send({'msg': 'could not retrieve villain'});
 
@@ -14,7 +14,7 @@ module.exports = function(app) {
     });
   });
 
-  app.post('/villains', function(req, res) {
+  app.post('/villains', eat_auth(appSecret), function(req, res) {
     var newVillain = new Villain(req.body);
     newVillain.save(function(err, villain) {
       if (err) return res.status(500).send({'msg':'could not save note'});
@@ -23,7 +23,7 @@ module.exports = function(app) {
     });
   });
 
-  app.put('/villains/:id', function(req, res) {
+  app.put('/villains/:id', eat_auth(appSecret), function(req, res) {
     var updatedVillain = req.body;
     delete updatedVillain._id;
     Villain.update({_id: req.params.id}, updatedVillain, function(err) {
@@ -33,7 +33,7 @@ module.exports = function(app) {
     });
   });
 
-  app.delete('/villains/:id', function(req, res) {
+  app.delete('/villains/:id', eat_auth(appSecret), function(req, res) {
     var deletedVillain;
     Villain.find({_id: req.params.id}, function(err, data) {
       if (err) return res.status(500).send({'msg': 'could not delete villain'});
